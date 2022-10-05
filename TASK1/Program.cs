ddb43cde84bcff99c1e52a5ad2a9e66780877099
Vera Lobova <pchilli@yandex.ru>
2022-10-05 16:14:41 +0300

    task1 done

---
+++ TASK1/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+// Var 1
+/*Console.WriteLine("Введите трехзначное целое число");
+int number = int.Parse (Console.ReadLine()!);
+int b = number.ToString().Length;
+if (b != 3)
+{
+    Console.WriteLine("Это не трехзначное число, перезапустите программу");
+}
+else
+{
+   int secondDigit = number/10%10;
+    Console.WriteLine(secondDigit);
+
+}*/
+
+// Var 2 - check incoming data for string 
+
+
+Console.WriteLine("Введите трехзначное целое число");
+var number = Console.ReadLine();
+
+int numberInt;
+    bool badIncome = int.TryParse(number, out numberInt);
+    if ( badIncome)
+    {
+        Console.WriteLine("Молодец, это число!");
+    }
+    else
+    {
+        Console.WriteLine("This is not a valid number!");
+        return;
+
+    }
+ int ourNumber = Convert.ToInt32(number);   
+
+
+if (ourNumber<0)
+{
+    ourNumber = -ourNumber;
+
+}
+int b = ourNumber.ToString().Length;
+Console.WriteLine($"length = {b}");
+
+if (b != 3)
+{
+    Console.WriteLine("Это не трехзначное число :( ");
+}
+else
+{
+    
+    int secondDigit = ourNumber/10%10;
+    Console.WriteLine($"Вторая цифра числа = {secondDigit}");
+
+}
