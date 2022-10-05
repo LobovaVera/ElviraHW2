b96fdc3579d08dfbdf861806daf048242486b40c
Vera Lobova <pchilli@yandex.ru>
2022-10-05 16:51:31 +0300

    Task2 done

---
+++ TASK2/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+Проверяет неправильный ввод - символы, 0, <10 , null, работает с отрицательными числами*/
+
+
+Console.WriteLine("Введите целое число");
+var number = Console.ReadLine();
+
+int numberInt;
+    bool badIncome = int.TryParse(number, out numberInt);
+    if ( badIncome)
+    {
+        Console.WriteLine("Молодец, это число!");
+    }
+    else
+    {
+        Console.WriteLine("This is not a valid number!");
+        return;
+
+    }
+ int ourNumber = Convert.ToInt32(number);   
+ if (ourNumber<0)
+{
+    ourNumber = -ourNumber;
+}
+    int b = ourNumber.ToString().Length;
+    if (b==0||b==1)
+    {
+        Console.WriteLine("Нет второй цифры :( ");
+        return;
+    }
+    
+    int thirdNumber =0;
+    for (int i= 0; i<b-3; i++)
+    {
+    ourNumber = ourNumber/10;
+
+    }
+     thirdNumber = ourNumber%10;
+       Console.WriteLine($"Третья цифра числа = {thirdNumber}");
